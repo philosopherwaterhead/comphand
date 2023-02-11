3bb7af22b948aeb96b902207f5ceb22262ae0dad
comphand <composershandle@gmail.com>
2023-02-11 22:55:43 +0900

    url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gainen.github.io',
+    site: 'https://philosopherwaterhead.github.io',
     base: '/comphand'
 });
