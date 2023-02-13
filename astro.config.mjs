12051c634afaa9c920005130e9b084c6c4ad8a4e
comphand <composershandle@gmail.com>
2023-02-14 04:05:05 +0900

    site fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://philosopherwaterhead.github.io',
+    site: 'https://comphand.pages.dev',
     base: '/comphand',
     markdown: {
         remarkPlugins: [
