eb26f5699497cdfb0611973a3a0713ef966e2924
comphand <composershandle@gmail.com>
2023-02-13 20:28:11 +0900

    katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,15 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://philosopherwaterhead.github.io',
-    base: '/comphand'
+    base: '/comphand',
+    markdown: {
+        remarkPlugins: [
+            'remark-math',
+        ],
+        rehypePlugins: [
+            ['rehype-katex', {
+            // Katex plugin options
+            }]
+        ]
+    }
 });
