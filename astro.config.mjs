78994993a4b6791982f3c9fba1b0e6167d1799d8
comphand <composershandle@gmail.com>
2024-01-31 01:55:01 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
             ['rehype-katex', {
             // Katex plugin options
             }]
-        ]   
-    }
+        ]
+        },
+    experimental: {
+        assets: true,
+    },
 });
