4d426fb5fc90b6de899cf41866c8ef88b49a70fe
comphand <composershandle@gmail.com>
2024-01-31 15:38:09 +0900

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://comphand.pages.dev',
-    base: '/comphand',
     markdown: {
         remarkPlugins: [
             'remark-math',
